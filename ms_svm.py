6ef297df64c719134236dcbef475445d1be36c6f
Maksym Konevych <konevichdeveloper@gmail.com>
2022-12-07 01:24:42 +0200

    First test prototype.

---
+++ ms_svm.py
@@ -0,0 +1,109 @@
+import statistics
+
+import numpy as np
+
+
+class Kernel:
+    def __call__(self, a, b):
+        ...
+
+
+class PolynomialKernel(Kernel):
+
+    def __init__(self, degree):
+        assert degree >= 1
+        self.degree = degree
+
+    def __call__(self, a, b):
+        # (a*b + 1) ** D
+        v = (a @ b + 1) ** self.degree
+        return v
+
+
+class RbfKernel(Kernel):
+
+    def __init__(self, sigma):
+        assert sigma >= 0
+        self.s = sigma
+
+    def __call__(self, a, b):
+        # exp(-||a-b||^2/s^2)
+        v = np.exp(- ((a-b) @ (a-b)) / self.s**2)
+        return v
+
+
+class Svm:
+    def __init__(self, kernel):
+        self.b = 0
+        self.x_train = np.array([])
+        self.y_train = np.array([])
+        self.a = np.array([])
+
+        self.ker = kernel
+
+        self.speed = 1e-3
+        self.e_s_n = 100     # early stopping
+
+    def __call__(self, x: np.ndarray):
+        if len(x.shape) == 1:
+            return sum(a_i*y_i*self.ker(x, x_i) for a_i, x_i, y_i in zip(self.a, self.x_train, self.y_train))
+        else:
+            return np.array([self(x[i]) for i in range(x.shape[0])])
+
+    def fit(self, x: np.ndarray, y: np.ndarray):
+
+        assert x.shape[0] == y.shape[0]
+
+        N = x.shape[0]
+        C = 10
+
+        self.x_train = x
+        self.y_train = y
+        self.a = np.ones(N)
+
+        n = 0
+        prev_L = np.inf
+        prev_a = self.a
+        while n < self.e_s_n:
+            # we minimize L = <d> - <s> + C*<r>, where
+            # <d> := 1/2 Sum(y_i*y_j * a_i*a_j * k(x_i, x_j), i = 1..N, j = 1..N)   -- double sum
+            # <s> := Sum(a_i, i = 1..N)                                             -- single sum
+            # <r> := |Sum(y_i * a_i, i = 1..N)|                                     -- restriction
+
+            d = sum(y[i] * y[j] * self.a[i] * self.a[j] * self.ker(x[i], x[j]) for i in range(N) for j in range(N))/2
+            s = sum(self.a[i] for i in range(N))
+            ri = sum(y[i] * self.a[i] for i in range(N))
+            r = abs(ri)
+
+            L = d - s + C*r
+
+            if L + 1e7 < prev_L:
+                prev_L = L
+                prev_a = self.a
+                n = 0
+            else:
+                n += 1
+
+            ddda = np.array([(
+                    y[i]*y[i]*self.a[i]*self.ker(x[i], x[i]) +
+                    sum(y[i]*y[j]*self.a[j]*self.ker(x[i], x[j])
+                        for j in range(N))) / 2
+                for i in range(N)])
+
+            dsda = np.ones_like(self.a)
+
+            drda = np.sign(ri) * np.array([y[i] for i in range(N)])     # should it me minus?
+
+            # final gradient of the loss function
+            dLda = ddda - dsda + C*drda
+
+            # applying the gradient
+            self.a -= self.speed * dLda
+
+        self.a = prev_a
+
+        # # finally, computing b
+        # self.b = statistics.median(abs(y[i] - sum(self.a[j]*y[j]*self.ker(x[i], x[j]) for j in range(N)))
+        #                            for i in range(N))
+
+
---
+++ test.py
@@ -0,0 +1,15 @@
+import ms_svm as mss
+import numpy as np
+
+svm = mss.Svm(mss.RbfKernel(.1))
+
+x = np.array([[1],
+              [1.9],
+              [2.1],
+              [3]])
+
+y = np.array([1, -1, -1, 1])
+
+svm.fit(x, y)
+
+print(svm(x))
